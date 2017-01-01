a6614af494501ef6860fae90a54f4f4559703f8f
Sergey Gernyak <sergeg1990@gmail.com>
2017-01-01 15:56:05 +0200

    Added session cntrl and index view

---
+++ web/controllers/session_controller.ex
@@ -0,0 +1,7 @@
+defmodule MoneyTracker.SessionController do
+  use MoneyTracker.Web, :controller
+
+  def index(conn, _params) do
+    render conn, "index.html"
+  end
+end
---
+++ web/views/session_view.ex
@@ -0,0 +1,4 @@
+defmodule MoneyTracker.SessionView do
+  use MoneyTracker.Web, :view
+end
+
