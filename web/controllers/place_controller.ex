531f07709602af6ddaa8502045fffc6038fe3e64
Sergey Gernyak <sergeg1990@gmail.com>
2017-01-02 19:42:50 +0200

    Basic CRUD actions has been done

--- web/controllers/place_controller.ex
+++ web/controllers/place_controller.ex
@@ -1,22 +1,26 @@
 defmodule MoneyTracker.PlaceController do
   use MoneyTracker.Web, :controller
+  use Guardian.Phoenix.Controller
+
+  import Ecto.Changeset, only: [put_change: 3]
 
   alias MoneyTracker.Place
 
   plug Guardian.Plug.EnsureAuthenticated, handler: __MODULE__
 
-  def index(conn, _params) do
-    places = Repo.all(Place)
+  def index(conn, _params, user, _claims) do
+    places = Place |> Place.for_user(user) |> Repo.all
     render(conn, "index.html", places: places)
   end
 
-  def new(conn, _params) do
+  def new(conn, _params, _user, _claims) do
     changeset = Place.changeset(%Place{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"place" => place_params}) do
-    changeset = Place.changeset(%Place{}, place_params)
+  def create(conn, %{"place" => place_params}, user, _claims) do
+    changeset = %Place{user_id: user.id}
+                |> Place.changeset(place_params)
 
     case Repo.insert(changeset) do
       {:ok, _place} ->
@@ -28,19 +32,19 @@ defmodule MoneyTracker.PlaceController do
     end
   end
 
-  def show(conn, %{"id" => id}) do
-    place = Repo.get!(Place, id)
+  def show(conn, %{"id" => id}, user, _claims) do
+    place = fetch_place(user, id)
     render(conn, "show.html", place: place)
   end
 
-  def edit(conn, %{"id" => id}) do
-    place = Repo.get!(Place, id)
+  def edit(conn, %{"id" => id}, user, _claims) do
+    place = fetch_place(user, id)
     changeset = Place.changeset(place)
     render(conn, "edit.html", place: place, changeset: changeset)
   end
 
-  def update(conn, %{"id" => id, "place" => place_params}) do
-    place = Repo.get!(Place, id)
+  def update(conn, %{"id" => id, "place" => place_params}, user, _claims) do
+    place = fetch_place(user, id)
     changeset = Place.changeset(place, place_params)
 
     case Repo.update(changeset) do
@@ -53,8 +57,8 @@ defmodule MoneyTracker.PlaceController do
     end
   end
 
-  def delete(conn, %{"id" => id}) do
-    place = Repo.get!(Place, id)
+  def delete(conn, %{"id" => id}, user, _claims) do
+    place = fetch_place(user, id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -65,9 +69,15 @@ defmodule MoneyTracker.PlaceController do
     |> redirect(to: place_path(conn, :index))
   end
 
-  def unauthenticated(conn, params) do
+  def unauthenticated(conn, _params) do
     conn
     |> put_flash(:error, "You must to authenticate first to see this page!")
     |> redirect(to: session_path(conn, :index))
+  end
+
+  defp fetch_place(user, place_id) do
+    Place
+    |> Place.for_user(user)
+    |> Repo.get!(place_id)
   end
 end
