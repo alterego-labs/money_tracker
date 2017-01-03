66408fd5ac13ac8e471c921a99b09f1ad987ac49
Sergey Gernyak <sergeg1990@gmail.com>
2017-01-03 18:20:18 +0200

    WIP: Show in the header current user menu
    When current user is guest a link to the sign in page is showen.
    For the authenticated

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,13 @@
 defmodule MoneyTracker.LayoutView do
   use MoneyTracker.Web, :view
+
+  import Plug.Conn
+
+  @doc """
+  Specifies if current user exists or not
+  """
+  @spec authenticated?(Plug.Conn.t) :: boolean
+  def authenticated?(conn) do
+    conn.assigns[:current_user] != nil
+  end
 end
