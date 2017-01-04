46a6c16483873c7d6f9f78c0a2bd153db6d711ac
Sergey Gernyak <sergeg1990@gmail.com>
2017-01-04 18:00:17 +0200

    Basic balances info on the dashboard page

--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -1,3 +1,20 @@
 defmodule MoneyTracker.PageView do
   use MoneyTracker.Web, :view
+
+  alias MoneyTracker.User
+  alias MoneyTracker.User.{AvailableCurrenciesFetcher, BalanceForCurrencyCalculator}
+
+  @type currency :: String.t
+  @type balance :: float
+
+  @doc """
+  Calculates and returns balance amount per currency for a particular user
+  """
+  @spec balances_per_currency(User.t) :: [{currency, balance}]
+  def balances_per_currency(%User{} = user) do
+    AvailableCurrenciesFetcher.run(user)
+    |> Enum.map(fn(currency) ->
+      {currency, BalanceForCurrencyCalculator.run(user, currency)}
+    end)
+  end
 end
