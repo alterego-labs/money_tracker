fc4b6d71c6de8b6420f774135a6b0945087273a9
Sergey Gernyak <sergeg1990@gmail.com>
2017-01-04 14:09:54 +0200

    Added Dashboard and Places link to the header

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -10,4 +10,14 @@ defmodule MoneyTracker.LayoutView do
   def authenticated?(conn) do
     conn.assigns[:current_user] != nil
   end
+
+  @spec dashboard_link_active?(Plug.Conn.t) :: boolean
+  def dashboard_link_active?(conn) do
+    conn.request_path == "/"
+  end
+
+  @spec place_link_active?(Plug.Conn.t) :: boolean
+  def place_link_active?(conn) do
+    Regex.match? ~r/\/places(\/?(\d+)?\/?(\?.*)?)$/, conn.request_path
+  end
 end
